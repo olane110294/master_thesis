eeda3af9fc14c71239a22d2fdec474c6de3f26fd
emilmsh <emilmshalseth@gmail.com>
2019-03-19 19:09:18 +0100

    update

--- expected_revenue.R
+++ expected_revenue.R
@@ -2,30 +2,41 @@
 
 # Input - From structural estimation #
 
-N = 10
+N = 31
 v0 = 0
-r = 0
-shape = 5
-scale = 1
+r = 1040.04191152325
+shape = 3
+scale = 1500
 
 # Expected Revenue
 
 interior = function(x){
-    (x * ((shape/scale) * (x/scale)^(shape - 1) * exp(-(x/scale)^shape)) +
-         (1 - exp(-(x / scale) ^ shape)) - 1) * ((1 - exp(-(x / scale) ^ shape)))^(N - 1)
+    (x * ((shape/scale) * (x/scale)^(shape - 1) * exp(-(x/scale)^shape)) -
+         exp(-(x / scale) ^ shape)) * ((1 - exp(-(x / scale) ^ shape)))^(N - 1)
 }
 
 Expected_Revenue_fun = function(N, v0, r, shape, scale) {
     ER = v0 * (1 - exp(-(r / scale) ^ shape)) ^ N +
-        N * integrate(interior, r, Inf)$value
+        N * integrate(interior, r, 100000)$value
     return(ER)
 }               
 
 Expected_Revenue_fun(N, v0, r, shape, scale)
 
-# Plot of Expected Revenue
-N_seq = seq(1,1000,by = 1)
-ExR = Expected_Revenue_fun(N_seq, v0, r, shape, scale)
+# Plot of Expected Revenue IKKE SIKKER PÅ OM DETTE STEMMER
+N_seq = seq(1,N,by = 1)
+
+ER_vec = sapply(N_seq, Expected_Revenue_fun, v0, r, shape, scale)
+ER_vec
+
 plot(N_seq,
-     ExR,
+     ER_vec,
      type = "l")
+
+# Test #
+
+test = function(x){
+    2*x
+}
+
+integrate(test, 0, 1 )
